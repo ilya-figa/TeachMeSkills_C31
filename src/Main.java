a05fadc746d73cb06cbe82f4e5be0b0830246582
Buyevich_I <ibuyevich@iba.by>
2024-08-07 15:02:15 +0300

    using Lesson1Tasks

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
+import lesson_1.Lesson1Tasks;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(Lesson1Tasks.getResulTask1(1,2));
+        System.out.println("_____________________________________");
+        System.out.println(Lesson1Tasks.getResulTask2(12));
+        System.out.println("_____________________________________");
+        System.out.println(Lesson1Tasks.getResulTask3(123));
+        System.out.println("_____________________________________");
+        System.out.println(Lesson1Tasks.getUniversalTask2And3(12));
+        System.out.println(Lesson1Tasks.getUniversalTask2And3(123));
+        System.out.println(Lesson1Tasks.getUniversalTask2And3(1234));
+        System.out.println("_____________________________________");
+        System.out.println(Lesson1Tasks.getResulTask4(1.2));
+        System.out.println(Lesson1Tasks.getResulTask4(1.8));
+        System.out.println("_____________________________________");
+        System.out.println(Lesson1Tasks.getResulTask5(21,8));
+        System.out.println("_____________________________________");
+        Lesson1Tasks.additionalTask_v1(1,2);
+        System.out.println("_____________________________________");
+        Lesson1Tasks.additionalTask_v2(1,2);
     }
 }
