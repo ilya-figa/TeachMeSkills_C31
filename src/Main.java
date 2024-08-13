be8a9556f0cf364f721b69885c1aa64a345feb03
Buyevich_IG <Ilya-figa.figa@yandex.ru>
2024-08-13 23:07:03 +0300

    using Lesson5Tasks

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
+import lesson_5.Lesson5Tasks;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("----------------------------TASK 1----------------------------");
+        Lesson5Tasks.resulTask1(scanner);
+        System.out.println("----------------------------TASK 2----------------------------");
+        Lesson5Tasks.resulTask2();
+        System.out.println("----------------------------Additional task----------------------------");
+        Lesson5Tasks.additionalTask(4,6);
     }
 }
